04020cab3c1960462d7a1779e876795eadd8e1a1
MikoAramu <108503031+MikoAramu@users.noreply.github.com>
2023-08-24 06:17:08 +0700

    Update inputnilai.blade.php

--- resources/views/pengurus/uploadnilai/inputnilai.blade.php
+++ resources/views/pengurus/uploadnilai/inputnilai.blade.php
@@ -2,63 +2,53 @@
 @section('content')
 
 <div class="adminx-content">
-        <!-- <div class="adminx-aside">
-
-        </div> -->
-
-        <div class="adminx-main-content">
-          <div class="container-fluid">
-            <!-- BreadCrumb -->
+    <div class="adminx-main-content">
+        <div class="container-fluid">
             <nav aria-label="breadcrumb" role="navigation">
-              <ol class="breadcrumb adminx-page-breadcrumb">
-                <li class="breadcrumb-item"><a href="#">Home</a></li>
-                <li class="breadcrumb-item active" aria-current="page">Nilai Perkuliahan</li>
-              </ol>
+                <ol class="breadcrumb adminx-page-breadcrumb">
+                    <li class="breadcrumb-item"><a href="#">Home</a></li>
+                    <li class="breadcrumb-item active" aria-current="page">Nilai Perkuliahan</li>
+                </ol>
             </nav>
 
             <div class="pb-3">
-              <h1>Silahkan masukkan nilai mahasiswa</h1>
+                <h1>Algoritma Pemrograman 2 </h1>
+                <p>Silahkan masukkan nilai mahasiswa</p>
             </div>
 
-            <table class="table table-bordered">
-                <thead>
-                    <tr>
-                        <th>No</th>
-                        <th>NPM</th>
-                        <th>Nama Mahasiswa</th>
-                        <th>Jurusan</th>
-                        <th>Semester</th>
-                        <th>Input Nilai</th>
-                        <th>Aksi</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach($datauser as $user)
-                    <tr>
-                        <td>{{ $loop->iteration }}</td>
-                        <td>{{ $user->npm }}</td>
-                        <td>{{ $user->name }}</td>
-                        <td>{{ $user->jurusan }}</td>
-                        <td>{{ $user->semester }}</td>
-                        <td>
-                            <form action="#" method="post">
-                                @csrf
-                                <input type="hidden" name="mahasiswa_id" value="{{ $user->id }}">
-                                <select name="matkul_id">
-                                    @foreach($dataMatakuliah as $mk)
-                                    <option value="{{ $mk->id }}">{{ $mk->nama_matkul }}</option>
-                                    @endforeach
-                                </select>
-                                <input type="number" name="nilai_kuliah" value="">
-                        </td>
-                        <td>
-                            <!-- Simpan tombol di sini -->
-                            <button type="submit" class="btn btn-primary">Simpan</button>
-                            </form>
-                        </td>
-                    </tr>
-                    @endforeach
-                </tbody>
-            </table>
+            <form action="{{ route('simpanNilai') }}" method="POST">
+                {{ csrf_field() }}
+                <input type="hidden" name="matkul_id" value="{{ $matkul_id }}">
+                <table class="table table-bordered">
+                    <thead>
+                        <tr>
+                            <th>No</th>
+                            <th>NPM</th>
+                            <th>Nama Mahasiswa</th>
+                            <th>Jurusan</th>
+                            <th>Semester</th>
+                            <th>Input Nilai</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach($dataUsers as $user)
+                        <tr>
+                            <td>{{ $loop->iteration }}</td>
+                            <td>{{ $user->mahasiswa->npm }}</td>
+                            <td>{{ $user->mahasiswa->nama }}</td>
+                            <td>{{ $user->mahasiswa->jurusan->nama_jurusan }}</td>
+                            <td>{{ $user->mahasiswa->semester->nama_semester }}</td>
+                            <td>
+                                <input type="number" name="nilai[{{ $user->mahasiswa->id }}]" placeholder="Masukkan nilai">
+                            </td>
+                        </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+                <button type="submit" name="button" class="btn btn-primary">Simpan Nilai</button>
+            </form>
+        </div>
+    </div>
+</div>
 
 @endsection

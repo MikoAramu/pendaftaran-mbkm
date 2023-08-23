28d0ffd4756260be5f8cf42800386fc05c9969aa
MikoAramu <108503031+MikoAramu@users.noreply.github.com>
2023-08-24 06:18:07 +0700

    Update NilaiMahasiswaPerkuliahanController.php

--- app/Http/Controllers/NilaiMahasiswaPerkuliahanController.php
+++ app/Http/Controllers/NilaiMahasiswaPerkuliahanController.php
@@ -1,10 +1,15 @@
 <?php
 
 namespace App\Http\Controllers;
-use Illuminate\Support\Facades\Auth;
+
 use Illuminate\Http\Request;
-Use App\user;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+
 Use App\MataKuliah;
+use App\User;
+use App\Mahasiswa;
+use App\NilaiMahasiswaPerkuliahan;
 
 class NilaiMahasiswaPerkuliahanController extends Controller
 {
@@ -19,17 +24,40 @@ class NilaiMahasiswaPerkuliahanController extends Controller
         return view('pengurus.uploadnilai.index', ['dataMatakuliah' => $dataMatakuliah]);
     }
 
-    public function inputNilai()
+    public function inputNilai(Request $request, $id)
     {
-        $datauser = User::where('role', 'mahasiswa')->get();
-
-        $dataMatakuliah = MataKuliah::with(['jurusan','semesters'])->get();
-
-        return view('pengurus.uploadnilai.inputnilai', [
-            'datauser' => $datauser,
-            'dataMatakuliah' => $dataMatakuliah
-        ]);
+        $dataUsers = User::where('role', 'mahasiswa')->get();
+        $npm = $request->npm; // Mendapatkan npm dari request
+        $matkul_id = $id; // Ambil nilai matkul_id dari parameter
+        return view('pengurus.uploadnilai.inputnilai', compact('dataUsers', 'id', 'npm', 'matkul_id'));
     }
+
+
+    public function simpanNilai(Request $request)
+    {
+        $request->validate([
+            'nilai.*' => 'required', // Validasi untuk semua nilai
+            'matkul_id' => 'required',
+        ]);
+    
+        $matkulId = $request->matkul_id;
+    
+        $nilaiData = []; // Menyimpan data nilai untuk disimpan nanti
+    
+        foreach ($request->nilai as $mahasiswaId => $nilai) {
+            $nilaiData[] = [
+                'mahasiswa_id' => $mahasiswaId,
+                'matkul_id' => $matkulId,
+                'nilai_kuliah' => $nilai,
+            ];
+        }
+    
+        if (NilaiMahasiswaPerkuliahan::insert($nilaiData)) {
+            return redirect()->route('inputNilai', ['id' => $matkulId])->with('success', 'Nilai berhasil disimpan');
+        } else {
+            return redirect()->route('inputNilai', ['id' => $matkulId])->with('error', 'Gagal menyimpan nilai');
+        }
+    }    
 
      public function editNilai()
     {
